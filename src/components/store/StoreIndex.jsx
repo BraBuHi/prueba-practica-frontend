ca432a817bf4674eb203d61d7b5d49a00bb24ef5
BraBuHi <brayhanstart@gmail.com>
2024-10-29 22:01:41 -0500

    Configuracion del from con esquema yup

--- src/components/store/StoreIndex.jsx
+++ src/components/store/StoreIndex.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from '../Navbar';
+import StoreForm from './storeForm';
 
 function StoreIndex() {
   return (
     <div>
       <NavBar />
       <h1>Store</h1>
+      <div className="max-w-7xl mx-auto p-4">
+        <StoreForm />
+        </div>
     </div>
 
   )
--- src/components/store/storeForm.jsx
+++ src/components/store/storeForm.jsx
@@ -8,8 +8,8 @@ const ProductForm = () => {
     // Define the validation schema
     const Productschema = yup.object({
         name: yup.string().required('Este campo es obligatorio'),
-        price: yup.number().positive('El precio debe ser un número positivo').required('Este campo es obligatorio'),
-        type: yup.string().oneOf(['Perecedero', 'No-Perecedero'], 'Selecciona un tipo válido').required('Este campo es obligatorio'),
+        city: yup.string().required('Este campo es obligatorio'),
+        address: yup.string().required('Este campo es obligatorio'),
     }).required();
 
     const { register, handleSubmit, formState: { errors } } = useForm({
@@ -22,16 +22,16 @@ const ProductForm = () => {
           //  const response = await axios.post('/api/products', data);
             console.log(response.data); // Handle the response as you wish
         } catch (error) {
-            console.error('Error al Agregar Producto:', error);
+            console.error('Error al Agregar Tienda:', error);
         }
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto">
-            <h2 className="text-xl font-bold mb-4">Agregar Producto</h2>
+            <h2 className="text-xl font-bold mb-4">Agregar La Tienda</h2>
 
             <div className="mb-4">
-                <label htmlFor="name" className="block text-gray-700">Nombre Del Producto:</label>
+                <label htmlFor="name" className="block text-gray-700">Nombre De La Tienda:</label>
                 <input
                     type="text"
                     id="name"
@@ -43,35 +43,34 @@ const ProductForm = () => {
             </div>
 
             <div className="mb-4">
-                <label htmlFor="price" className="block text-gray-700">Precio:</label>
+                <label htmlFor="city" className="block text-gray-700">Ciudad:</label>
                 <input
-                    type="number"
-                    id="price"
-                    {...register('price')}
+                    type="text"
+                    id="city"
+                    {...register('city')}
                     className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-orange-500 focus:ring focus:ring-orange-200"
-                    placeholder='0'
+                    placeholder='Example'
                 />
-                {errors.price && <span className="text-red-500">{errors.price.message}</span>}
+                {errors.city && <span className="text-red-500">{errors.city.message}</span>}
             </div>
 
             <div className="mb-4">
-                <label htmlFor="type" className="block text-gray-700">Tipo De Producto:</label>
-                <select
-                    id="type"
-                    {...register('type')}
+                <label htmlFor="address" className="block text-gray-700">Direccion</label>
+                <input
+                    type="text"
+                    id="address"
+                    {...register('address')}
                     className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-orange-500 focus:ring focus:ring-orange-200"
-                >
-                    <option value="Perecedero">Perecedero</option>
-                    <option value="No-Perecedero">No Perecedero</option>
-                </select>
-                {errors.type && <span className="text-red-500">{errors.type.message}</span>}
+                    placeholder='Example'
+                />
+                {errors.address && <span className="text-red-500">{errors.address.message}</span>}
             </div>
 
             <button
                 type="submit"
                 className="bg-orange-600 text-white px-4 py-2 rounded-lg hover:bg-orange-700 transition duration-300"
             >
-                Agregar Producto
+                Agregar Tienda
             </button>
         </form>
     );
