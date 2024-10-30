9063e5979724404c730b18765aa26dfdbc362c60
BraBuHi <brayhanstart@gmail.com>
2024-10-29 23:28:37 -0500

    agrego lista de tiendas

--- src/components/store/StoreIndex.jsx
+++ src/components/store/StoreIndex.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import NavBar from '../Navbar';
 import StoreForm from './storeForm';
+import StoreList from './storeList';
 
 function StoreIndex() {
+    const stores=[
+        {
+          id:1,
+          name:"tienda1",
+          city:"Armenia"
+        },
+        {
+            id:2,
+            name:"tienda2",
+            city:"Armenia"
+          },
+          {
+            id:3,
+            name:"tienda3",
+            city:"Armenia"
+          },
+          {
+            id:4,
+            name:"tienda4",
+            city:"Armenia"
+          },
+      ]
+
   return (
     <div>
       <NavBar />
       <div className="max-w-7xl mx-auto p-4">
-        <h1>Lista De Tiendas</h1>
         <a href="/tiendas/crear">Crear Tienda</a>
+        <StoreList stores={stores}/>
         </div>
     </div>
 
